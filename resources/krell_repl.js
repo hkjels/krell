2f332cf4744401083265d47ff05d2e178106007f
Henrik Kjerringvåg <henrik@kjerringvag.no>
2022-05-02 21:48:14 +0200

    Use `Web` as `device-id` if `device-info` fails to report
    This is needed in order to target web-builds in the `krellPortMap`.

--- resources/krell_repl.js
+++ resources/krell_repl.js
@@ -1,189 +1,196 @@
 import { Platform } from "react-native";
 import TcpSocket from "react-native-tcp-socket";
 import DeviceInfo from "react-native-device-info";
-import { krellPortMap } from '../app.json';
+import { krellPortMap } from "../app.json";
 import {
-    bootstrapRepl,
-    evaluate,
-    loadFile,
-    onSourceLoad
+  bootstrapRepl,
+  evaluate,
+  loadFile,
+  onSourceLoad,
 } from "./closure_bootstrap.js";
+
+const deviceId = DeviceInfo.getDeviceId() || "Web";
 
 var CONNECTED = false;
 var RECONNECT_INTERVAL = 3000;
 
 var SERVER_IP = "$KRELL_SERVER_IP";
-var SERVER_PORT = krellPortMap ? krellPortMap[DeviceInfo.getDeviceId()] || $KRELL_SERVER_PORT : $KRELL_SERVER_PORT;
+var SERVER_PORT = krellPortMap
+  ? krellPortMap[deviceId] || $KRELL_SERVER_PORT
+  : $KRELL_SERVER_PORT;
 
 const KRELL_VERBOSE = $KRELL_VERBOSE;
 
 var reloadListeners = [];
 
-console.log("Krell sez howdy, Device ID:", DeviceInfo.getDeviceId());
+console.log("Krell sez howdy, Device ID:", deviceId);
 
 // =============================================================================
 // REPL Server
 
 const exists_ = (obj, xs) => {
-    if(typeof xs == "string") {
-        xs = xs.split(".");
-    }
-    if(xs.length >= 1) {
-        let key = xs[0],
-            hasKey = obj.hasOwnProperty(key);
-        if (xs.length === 1) {
-            return hasKey;
-        } else {
-            if(hasKey) {
-                return exists_(obj[key], xs.slice(1));
-            }
-        }
-        return false;
+  if (typeof xs == "string") {
+    xs = xs.split(".");
+  }
+  if (xs.length >= 1) {
+    let key = xs[0],
+      hasKey = obj.hasOwnProperty(key);
+    if (xs.length === 1) {
+      return hasKey;
     } else {
-        return false;
+      if (hasKey) {
+        return exists_(obj[key], xs.slice(1));
+      }
     }
+    return false;
+  } else {
+    return false;
+  }
 };
 
 const notifyReloadListeners = () => {
-    reloadListeners.forEach(function(x) {
-        x();
-    });
+  reloadListeners.forEach(function (x) {
+    x();
+  });
 };
 
 const onKrellReload = (cb) => {
-    reloadListeners.push(cb);
+  reloadListeners.push(cb);
 };
 
 const errString = (err) => {
-    if(typeof cljs !== "undefined") {
-        if(typeof cljs.repl !== "undefined") {
-            cljs.repl.error__GT_str(err)
-        } else {
-            return err.toString();
-        }
+  if (typeof cljs !== "undefined") {
+    if (typeof cljs.repl !== "undefined") {
+      cljs.repl.error__GT_str(err);
     } else {
-        return err.toString();
+      return err.toString();
     }
+  } else {
+    return err.toString();
+  }
 };
 
 const handleMessage = (socket, data) => {
-    var err = null,
-        ret = null,
-        msg = null;
+  var err = null,
+    ret = null,
+    msg = null;
 
-    data = data.replace(/\0/g, "");
+  data = data.replace(/\0/g, "");
 
-    try {
-        msg = JSON.parse(data);
-        if(msg.form) {
-            ret = evaluate(msg.form);
-        }
-    } catch (e) {
-        if(msg && msg.form) {
-            console.error("Could not evaluate form:", msg.form, e);
-        } else {
-            console.error("Invalid message:", msg, e);
-        }
-        err = e;
+  try {
+    msg = JSON.parse(data);
+    if (msg.form) {
+      ret = evaluate(msg.form);
     }
-
-    if (err) {
-        socket.write(
-            JSON.stringify({
-                type: "result",
-                status: "exception",
-                value: errString(err)
-            })+"\0"
-        );
+  } catch (e) {
+    if (msg && msg.form) {
+      console.error("Could not evaluate form:", msg.form, e);
     } else {
-        if (ret !== undefined && ret !== null) {
-            socket.write(
-                JSON.stringify({
-                    type: "result",
-                    status: "success",
-                    value: ret.toString(),
-                })+"\0"
-            );
-        } else {
-            socket.write(
-                JSON.stringify({
-                    type: "result",
-                    status: "success",
-                    value: null,
-                })+"\0"
-            );
-        }
+      console.error("Invalid message:", msg, e);
     }
+    err = e;
+  }
+
+  if (err) {
+    socket.write(
+      JSON.stringify({
+        type: "result",
+        status: "exception",
+        value: errString(err),
+      }) + "\0"
+    );
+  } else {
+    if (ret !== undefined && ret !== null) {
+      socket.write(
+        JSON.stringify({
+          type: "result",
+          status: "success",
+          value: ret.toString(),
+        }) + "\0"
+      );
+    } else {
+      socket.write(
+        JSON.stringify({
+          type: "result",
+          status: "success",
+          value: null,
+        }) + "\0"
+      );
+    }
+  }
 };
 
-global.KRELL_RELOAD = async function(nses) {
-    for(let ns of nses) {
-        let path = goog.debugLoader_.getPathFromDeps_(ns);
-        await loadFile(path);
-        console.log("Reloaded", ns);
-    }
-    notifyReloadListeners();
-}
+global.KRELL_RELOAD = async function (nses) {
+  for (let ns of nses) {
+    let path = goog.debugLoader_.getPathFromDeps_(ns);
+    await loadFile(path);
+    console.log("Reloaded", ns);
+  }
+  notifyReloadListeners();
+};
 
 const initSocket = (socket) => {
-    var buffer = "";
+  var buffer = "";
 
-    socket.on("data", data => {
-        if (data[data.length - 1] !== 0) {
-            buffer += data;
-        } else {
-            data = buffer + data;
-            buffer = "";
-            handleMessage(socket, data);
-        }
-    });
+  socket.on("data", (data) => {
+    if (data[data.length - 1] !== 0) {
+      buffer += data;
+    } else {
+      data = buffer + data;
+      buffer = "";
+      handleMessage(socket, data);
+    }
+  });
 
-    socket.on("error", error => {
-        socket.destroy();
-        //console.log("An error occurred with client socket:", error);
-    });
+  socket.on("error", (error) => {
+    socket.destroy();
+    //console.log("An error occurred with client socket:", error);
+  });
 
-    socket.on("close", error => {
-        socket.destroy();
-        if (CONNECTED) {
-            console.log("Closed connection with", socket.address());
-        }
-        CONNECTED = false;
-        setTimeout(tryConnection, RECONNECT_INTERVAL);
-    });
+  socket.on("close", (error) => {
+    socket.destroy();
+    if (CONNECTED) {
+      console.log("Closed connection with", socket.address());
+    }
+    CONNECTED = false;
+    setTimeout(tryConnection, RECONNECT_INTERVAL);
+  });
 };
 
 // Loop to connect from client to server
 const tryConnection = () => {
-    if(!CONNECTED) {
-        var socket = TcpSocket.createConnection({
-            host: SERVER_IP,
-            port: SERVER_PORT,
-            tls: false,
-        }, function(address) {
-            console.log("Connected to Krell REPL Server");
-            CONNECTED = true;
-            // once cljs.core loaded, can monkey-patch Closure and setup printing
-            if(exists_(global, "cljs.core")) {
-                bootstrapRepl(socket);
-            } else {
-                onSourceLoad(goog.debugLoader_.getPathFromDeps_("cljs.core"), () => {
-                    bootstrapRepl(socket);
-                });
-            }
-        });
-        initSocket(socket);
-    } else {
-        setTimeout(tryConnection, RECONNECT_INTERVAL);
-    }
+  if (!CONNECTED) {
+    var socket = TcpSocket.createConnection(
+      {
+        host: SERVER_IP,
+        port: SERVER_PORT,
+        tls: false,
+      },
+      function (address) {
+        console.log("Connected to Krell REPL Server");
+        CONNECTED = true;
+        // once cljs.core loaded, can monkey-patch Closure and setup printing
+        if (exists_(global, "cljs.core")) {
+          bootstrapRepl(socket);
+        } else {
+          onSourceLoad(goog.debugLoader_.getPathFromDeps_("cljs.core"), () => {
+            bootstrapRepl(socket);
+          });
+        }
+      }
+    );
+    initSocket(socket);
+  } else {
+    setTimeout(tryConnection, RECONNECT_INTERVAL);
+  }
 };
 
-if(!process.env["KRELL_NO_REPL"]) {
-    tryConnection();
+if (!process.env["KRELL_NO_REPL"]) {
+  tryConnection();
 }
 
 module.exports = {
-    evaluate: evaluate,
-    onKrellReload: onKrellReload,
-    onSourceLoad: onSourceLoad
+  evaluate: evaluate,
+  onKrellReload: onKrellReload,
+  onSourceLoad: onSourceLoad,
 };
